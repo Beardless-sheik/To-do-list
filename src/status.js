d1bcf31da1094413918f8b526a937480a2e886c9
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-08-24 14:24:30 +0200

    Fix linter errors

--- src/status.js
+++ src/status.js
@@ -4,6 +4,5 @@ export default function checkBoxEventListener(parentCheckBoxContainer, tasksArra
     const index = tasksArray.findIndex((element) => element.index === parseInt(target.id, 10));
     tasksArray[index].completed = !tasksArray[index].completed;
     localStorage.setItem('TasksList', JSON.stringify(tasksArray));
-    console.log(tasksArray);
   });
 }

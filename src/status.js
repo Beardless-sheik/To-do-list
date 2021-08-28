baccff63c73dba3915241b3a6157b6c3ccdb771a
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-08-28 23:25:42 +0200

    Update status.js

--- src/status.js
+++ src/status.js
@@ -1,4 +1,4 @@
-export default function checkBoxEventListener(parentCheckBoxContainer, tasksArray) {
+function checkBoxEventListener(parentCheckBoxContainer, tasksArray) {
   parentCheckBoxContainer.addEventListener('change', (event) => {
     const { target } = event;
     const index = tasksArray.findIndex((element) => element.index === parseInt(target.id, 10));
@@ -6,3 +6,20 @@ export default function checkBoxEventListener(parentCheckBoxContainer, tasksArra
     localStorage.setItem('TasksList', JSON.stringify(tasksArray));
   });
 }
+
+function editTaskEventListener(parentCheckBoxContainer, tasksArray) {
+  parentCheckBoxContainer.addEventListener('click', (event) => {
+    const { target } = event;
+    const descriptionText = target.previousElementSibling;
+    // Toggle contentEditable on button click
+   	descriptionText.contentEditable = !descriptionText.isContentEditable;
+
+    // If disabled, save text
+   if(descriptionText.contentEditable === 'false') {
+   	// localStorage.setItem('content', content.innerHTML);
+     console.log(descriptionText.id);
+   } 
+  });
+}
+
+export { checkBoxEventListener, editTaskEventListener};

0782e9effac2de5e54fc6ca46d32cc16aed632e8
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-08-28 23:25:30 +0200

    Update index.js
    Add local storage
    Add event listeners for tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
-import checkBoxEventListener from './status.js';
+import { checkBoxEventListener, editTaskEventListener } from './status.js';
+import Update from './crud.js';
 
 let tasks = [{
   description: 'These are items for Task 1 - index - 2',
@@ -18,13 +19,19 @@ let tasks = [{
 }];
 
 const bookList = document.getElementById('bookList');
+const input = document.getElementById('task-input');
 
+const update = new Update();
 const getLocalStorageTasks = () => {
   const getData = localStorage.getItem('TasksList');
   const data = JSON.parse(getData);
   if (data) {
     tasks = data;
   }
+};
+
+const pushLocalStorageTasks = () => {
+  localStorage.setItem('TasksList', JSON.stringify(tasks));
 };
 
 const sortTasks = (tasksArray) => {
@@ -37,18 +44,20 @@ const populatelist = () => {
   tasks.forEach((tasks) => {
     if (tasks.completed === true) {
       bookList.innerHTML += `
-    <li class="list-item">
+    <li class="list-item" draggable="true">
     <input type="checkbox" class="position-grid-start" id="${tasks.index}" checked/>
-      <p> ${tasks.description} </p>
+      <p contenteditable="true"> ${tasks.description} </p>
       <i class="fas fa-ellipsis-v position-grid-end"></i>
+      <i class="fas fa-trash-alt position-grid-end"></i>
     <li>   
     `;
     } else {
       bookList.innerHTML += `
-    <li class="list-item">
+    <li class="list-item" draggable="true">
     <input type="checkbox" class="position-grid-start" id="${tasks.index}"/>
       <p> ${tasks.description} </p>
       <i class="fas fa-ellipsis-v position-grid-end"></i>
+      <i class="fas fa-trash-alt position-grid-end"></i>
     <li>   
     `;
     }
@@ -59,5 +68,30 @@ const populatelist = () => {
 document.addEventListener('DOMContentLoaded', () => {
   getLocalStorageTasks();
   checkBoxEventListener(bookList, tasks);
+  editTaskEventListener(bookList, tasks);
+
+  // editBtn.addEventListener('click', () => {
+
+  //   // Toggle contentEditable on button click
+  // 	content.contentEditable = !content.isContentEditable;
+
+//   // If disabled, save text
+//   if(content.contentEditable === 'false') {
+//   	localStorage.setItem('content', content.innerHTML);
+//   }
+// });
 });
 document.addEventListener('DOMContentLoaded', populatelist);
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const newTask = {
+      description: input.value,
+      completed: false,
+      index: tasks.length + 1,
+    };
+    console.log(newTask);
+    tasks = update.add(newTask, tasks);
+    pushLocalStorageTasks();
+    populatelist();
+  }
+});

6fe0a424f3f1b88fdfba8c2be372008d1735dd04
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-09-25 11:02:43 +0200

    Add checks on event listener to sync tasks with local storage

--- src/status.js
+++ src/status.js
@@ -1,25 +1,12 @@
-function checkBoxEventListener(parentCheckBoxContainer, tasksArray) {
+function checkBoxEventListener(parentCheckBoxContainer) {
+  const tasksArray = JSON.parse(localStorage.getItem('tasksList'));
   parentCheckBoxContainer.addEventListener('change', (event) => {
     const { target } = event;
     const index = tasksArray.findIndex((element) => element.index === parseInt(target.id, 10));
     tasksArray[index].completed = !tasksArray[index].completed;
-    localStorage.setItem('TasksList', JSON.stringify(tasksArray));
+    console.log(tasksArray);
+    localStorage.setItem('tasksList', JSON.stringify(tasksArray));
   });
 }
 
-function editTaskEventListener(parentCheckBoxContainer, tasksArray) {
-  parentCheckBoxContainer.addEventListener('click', (event) => {
-    const { target } = event;
-    const descriptionText = target.previousElementSibling;
-    // Toggle contentEditable on button click
-   	descriptionText.contentEditable = !descriptionText.isContentEditable;
-
-    // If disabled, save text
-   if(descriptionText.contentEditable === 'false') {
-   	// localStorage.setItem('content', content.innerHTML);
-     console.log(descriptionText.id);
-   } 
-  });
-}
-
-export { checkBoxEventListener, editTaskEventListener};
+export { checkBoxEventListener };

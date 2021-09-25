b4410105b25b6b65641450850935ef0043313eb3
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-09-25 11:07:37 +0200

    create add, edit and clear all comleted task functionality into index file

--- src/crud.js
+++ src/crud.js
@@ -1,13 +1,60 @@
-class Update {
-  constructor() {
+// eslint-disable-next-line import/no-cycle
+// import { populatelist } from './index.js';
 
-  }
-
-  add(task, taskArray) {
-    taskArray = [...taskArray, task];
-    console.log(task);
-    return taskArray;
-  }
+function editTaskEventListener(parentCheckBoxContainer) {
+  parentCheckBoxContainer.addEventListener('input', (event) => {
+    const { target } = event;
+    console.log(target.classList.value);
+    if (target.classList.value.indexOf('editable') !== -1) {
+      const tasksArray = JSON.parse(localStorage.getItem('tasksList'));
+      const inputWithId = target.previousElementSibling;
+      console.log(target);
+      // eslint-disable-next-line max-len
+      const index = tasksArray.findIndex((element) => element.index === parseInt(inputWithId.id, 10));
+      tasksArray[index].description = target.innerHTML;
+      localStorage.setItem('tasksList', JSON.stringify(tasksArray));
+    }
+  });
 }
 
-export default Update;
+const representNewIndexes = (tasksArray) => {
+  tasksArray.forEach((element, index) => {
+    element.index = index;
+  });
+};
+
+const addNewTask = (newTask, incomingTasks) => {
+  let newTaskArray;
+  if (incomingTasks) {
+    newTaskArray = [...incomingTasks, newTask];
+  } else {
+    newTaskArray.push(newTask);
+  }
+  representNewIndexes(newTaskArray);
+  return newTaskArray;
+};
+
+const deleteTask = (index, tasksArray) => {
+  const filteredArray = tasksArray.filter((element) => element.index !== index);
+  representNewIndexes(filteredArray);
+  console.log(filteredArray);
+  return filteredArray;
+};
+
+const clearAllCompletedTasks = (completedButtonElement) => {
+  completedButtonElement.addEventListener('click', (event) => {
+    if (event.target.id === 'clear-button') {
+      const tasksArray = JSON.parse(localStorage.getItem('tasksList'));
+      console.log(tasksArray);
+      const filteredArray = tasksArray.filter((element) => element.completed === false);
+      representNewIndexes(filteredArray);
+      localStorage.setItem('tasksList', JSON.stringify(filteredArray));
+      // eslint-disable-next-line no-restricted-globals
+      location.reload();
+    }
+  });
+};
+
+export {
+  editTaskEventListener, addNewTask, deleteTask, clearAllCompletedTasks,
+};

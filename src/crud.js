3aff3784a9c5bfcf5a523109e335259274735598
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-11 23:25:44 +0200

    Implement index changes upon deletion of tasks in taskArray

--- src/crud.js
+++ src/crud.js
@@ -1,12 +1,17 @@
 const addNewTask = (tasksArray, newTask) => {
   const newTaskArray = [...tasksArray, newTask];
+  console.log(newTaskArray);
   return newTaskArray;
-}
+};
 
 const deletingTask = (tasksArray, deletedTaskIndex) => {
   const newTaskArray = tasksArray.filter((task) => task.index !== deletedTaskIndex);
+  newTaskArray.forEach((task, index) => {
+    const tempIndex = Number(index + 1);
+    task.index = tempIndex;
+  });
   console.log(newTaskArray);
   return newTaskArray;
-}
+};
 
-export {addNewTask, deletingTask};
+export { addNewTask, deletingTask };

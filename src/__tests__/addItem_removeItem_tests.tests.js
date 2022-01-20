ed828f7e031a5c179dcf2dfb9e841a78a91630a6
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-20 09:47:59 +0200

    Add tests for delete function

--- src/__tests__/addItem_removeItem_tests.tests.js
+++ src/__tests__/addItem_removeItem_tests.tests.js
@@ -19,3 +19,23 @@ describe('tests to run for add function', ()=> {
     })[0].description).toBe('string');
   });
 });
+
+describe('tests to run for delete function', () => {
+  const taskArray = [{
+    description: 'test description 1',
+    completed: false,
+    index: 1,
+  },{
+    description: 'test description 2',
+    completed: false,
+    index: 2,
+  }];
+
+  test('check if length of array will be reduced by 1 after deletion', () => {
+    expect(deletingTask(taskArray, 1).length).toBe(1);
+  });
+
+  test('check if length of array after removing last task will be zero', () => {
+    expect(deletingTask(taskArray, 1).length).toBe(0);
+  })
+})

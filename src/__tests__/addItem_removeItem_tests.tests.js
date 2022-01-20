ac7cd0d0cee0b69081a49c9272398ca661478d35
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-01-20 11:18:45 +0100

    Add editingTask test

--- src/__tests__/addItem_removeItem_tests.tests.js
+++ src/__tests__/addItem_removeItem_tests.tests.js
@@ -1,35 +1,42 @@
-import { addNewTask, deletingTask } from '../crud.js';
+import { addNewTask, deletingTask, editingTask } from '../crud.js';
 
 describe('tests to run for add function', () => {
   const taskArray = [];
 
   test('check length of task array after adding a new task', () => {
-    expect(addNewTask(taskArray, {
-      description: 'test description 1',
-      completed: false,
-      index: 1,
-    }).length).toBe(1);
+    expect(
+      addNewTask(taskArray, {
+        description: 'test description 1',
+        completed: false,
+        index: 1,
+      }).length
+    ).toBe(1);
   });
 
   test('check whether description added to the array is a string', () => {
-    expect(typeof addNewTask(taskArray, {
-      description: 'test description 1',
-      completed: false,
-      index: 1,
-    })[0].description).toBe('string');
+    expect(
+      typeof addNewTask(taskArray, {
+        description: 'test description 1',
+        completed: false,
+        index: 1,
+      })[0].description
+    ).toBe('string');
   });
 });
 
 describe('tests to run for delete function', () => {
-  const taskArray = [{
-    description: 'test description 1',
-    completed: false,
-    index: 1,
-  }, {
-    description: 'test description 2',
-    completed: false,
-    index: 2,
-  }];
+  const taskArray = [
+    {
+      description: 'test description 1',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'test description 2',
+      completed: false,
+      index: 2,
+    },
+  ];
 
   test('check if length of array will be reduced by 1 after deletion', () => {
     expect(deletingTask(taskArray, 1).length).toBe(1);
@@ -37,5 +44,26 @@ describe('tests to run for delete function', () => {
 
   test('check if length of array after removing last task will be zero', () => {
     expect(deletingTask(taskArray, 1).length).toBe(0);
+  });
+});
+
+describe('tests for task editing', () => {
+  const taskArray = [
+    {
+      description: 'test description 1',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'test description 2',
+      completed: false,
+      index: 2,
+    },
+  ];
+
+  test('check if task is editable', () => {
+    const editTaskDescription = 'New description 2';
+    const task = editingTask(taskArray, 2, editTaskDescription);
+    expect(task[1].description).toMatch('New description 2');
   });
 });
